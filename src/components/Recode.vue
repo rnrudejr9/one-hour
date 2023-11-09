124977622370a9ae42150c4ca307f6540dd8bba2
rnrudejr9 <rnrudejr1234@naver.com>
2023-11-09 11:30:29 +0900

    UI: Recode저장 화면 구성

--- src/components/Recode.vue
+++ src/components/Recode.vue
@@ -1,10 +1,42 @@
 <script setup>
 const props = defineProps(["recode"]);
+const recoder = props.recode;
 </script>
 <template>
   <div><h3>Recode Area</h3></div>
 
-  {{ props }}
+  <h2>Card</h2>
+
+  <div class="card">
+    <img src="img_avatar.png" alt="Avatar" style="width: 100%" />
+    <div class="container">
+      <h4>
+        <div v-for="t in recoder.text">
+          <b>{{ t }}</b>
+        </div>
+      </h4>
+
+      <div>start : {{ recoder.startTime }}</div>
+      <div>end : {{ recoder.endTime }}</div>
+      <div>걸린시간 : {{ recoder.takeTime }}</div>
+      <div>선택시간 : {{ recoder.chooseTime }}</div>
+      <div>멈춘횟수 : {{ recoder.stopCount }}</div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 40%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
