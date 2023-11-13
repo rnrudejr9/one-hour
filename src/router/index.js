d7c9411724232825f833c7442916d4947dc5b540
koopi <49141751+rnrudejr9@users.noreply.github.com>
2023-11-13 15:44:34 +0900

    feat: 라우팅 기능, 목표 저장 기능, 중도 포기 기능 구현 (#13)
    * UI: Recode저장 화면 구성
    
    * feat: 중도포기 기능, 시작 시 버튼 비활활성화 기능 구현
    
    * docs: readme 수정
    
    * fix: deploy 문제 해결
    
    * feat: 라우팅 기능 구현 (#11)
    
    * fix: vue-router 설치
    
    * feat: 설명서,사용법 뷰
    
    * feat: 라우터 기능 추가
    
    * feat : 라우터로 변경, 라우터링크 css 폰트, 커서모양 변경
    
    ---------
    
    Co-authored-by: SeokJu <116260619+SeokJuGo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SeokJu <116260619+SeokJuGo@users.noreply.github.com>

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import ManualHourStudy from "../components/instruction/ManualHourStudy.vue";
+import HowToUse from "../components/instruction/HowToUse.vue"
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/manual",
+      name: "manual",
+      component: ManualHourStudy,
+    },
+    {
+      path: "/how",
+      name: "how",
+      component: HowToUse,
+    },
+  ],
+});
+
+export default router;
