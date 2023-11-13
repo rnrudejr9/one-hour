4ada89262ebe7af14e99a4df95777d869d0dd592
rnrudejr9 <rnrudejr1234@naver.com>
2023-11-13 17:01:52 +0900

    fix: 라우팅 문제 해결

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import ManualHourStudy from "@/components/instruction/ManualHourStudy.vue";
 import HowToUse from "@/components/instruction/HowToUse.vue";
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/manual",
